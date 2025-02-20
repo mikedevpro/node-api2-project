9dd44d0acab4f103bce17dadc1d5fbf0e353f700
Michael Nobles <Mnobles33@gmail.com>
2025-02-19 19:11:57 -0700

    Next commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,27 @@
 // implement your posts router here
 const express = require('express')
-// const Post = require('./posts-model')
+
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.json('foo')
+})
+router.get('/:id', (req, res) => {
+  
+})
+router.post('/', (req, res) => {
+  
+})
+router.delete('/:id', (req, res) => {
+  
+})
+router.put('/:id', (req, res) => {
+  
+})
+router.get('/:id/messages', (req, res) => {
+  
+})
+
+
 
 module.exports = router

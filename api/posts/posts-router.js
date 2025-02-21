d39f302648f1bbf3cb00cccf5fea009c3872f732
Michael Nobles <Mnobles33@gmail.com>
2025-02-21 16:23:25 -0700

    Last commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -112,8 +112,24 @@ router.put('/:id', (req, res) => {
       })
   }
 })
-router.get('/:id/messages', (req, res) => {
-  
+router.get('/:id/comments', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      })
+    } else {
+      const comments = await Post.findCommentById(req.params.id)
+      res.json(comments)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The comments information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    })
+  }
 })
 
 module.exports = router
